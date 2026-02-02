ff2bb5e6a8245221b2ee4bc4ac9db79412d92c8d
ageless <71825899+agelesskid@users.noreply.github.com>
2026-02-02 23:01:23 +0100

    Update location and personal description in About component

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,18 +2,11 @@ export default function About() {
     return (
         <section id="about">
             <h2 className="about-main-heading section-main-heading">Who I am</h2>
-            <p className="about-main-desc section-main-desc">Front-End Developer and tea lover from Kyiv, Ukraine</p>
+            <p className="about-main-desc section-main-desc">Front-End Developer and tea lover from Bremen, Germany</p>
            
             <div className="about-secondary-desc-wrapper section-secondary-desc-wrapper">
                 <p>
-                    I studied Design Engineering a couple of years ago but then decided to switch to studying Web Development at a different university.
-                    I soon discovered my love for Front-End Web Development.
-                    Unfortunately, I had to leave university due to the outbreak of war.
-                    However, I recently decided to enroll in the Scrimba Front-End Development Path to further my skills.
-                    My tech stack includes React JS, Vanilla JS, HTML, and CSS, and I am proficient in working with Figma. 
-                </p>
-                <p>
-                    Currently, I live in Bremen, Germany.
+                    My tech stack includes React JS, Vanilla JS, HTML, and CSS, and I am proficient in working with Figma.
                 </p>
                 <p>
                     Outside of education, I enjoy drinking tea and spending time with my cat :)
