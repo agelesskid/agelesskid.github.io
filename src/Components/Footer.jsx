cb9d43ecd5d2da3e1bd98d19c821577e60b5b9eb
ageless <agelesskid@proton.me>
2026-02-04 18:09:54 +0100

    deleted unused imports; added new contact link (email)

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGithub, faLinkedin, faTelegram } from "@fortawesome/free-brands-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 export default function Footer() {
     return (
         <footer id="contact">
             <div className="footer-item">
-                <h2 className="footer-heading">Sviatoslav "ageless" Kulieshov</h2>
+                <h2 className="footer-heading">Sviatoslav Kulieshov</h2>
                 <p className="footer-desc">Please reach out if you have any questions! I would gladly jump on a video call to speak with you!</p>
             </div>
             <ul className="footer-links-list links-wrapper">
+                <li className="footer-links-list-item"><Link to="mailto:agelesskid@proton.me"><FontAwesomeIcon icon={faEnvelope} className="fa-2x"/></Link></li>
                 <li className="footer-links-list-item"><Link to="https://github.com/agelesskid"><FontAwesomeIcon icon={faGithub} className="fa-2x"/></Link></li>
             </ul>
         </footer>
