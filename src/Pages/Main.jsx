891a63e31fe3221b4fd617b34fe60238f04b861b
ageless <agelesskid@proton.me>
2026-02-04 18:11:03 +0100

    changed main page structure

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,7 +1,5 @@
 import Header from "../Components/Header"
 import Footer from "../Components/Footer"
-import Home from "../Components/Home"
-import About from "../Components/About"
 import Projects from "../Components/Projects"
 
 export default function Main() {
@@ -9,8 +7,6 @@ export default function Main() {
         <>
             <Header />
             <main>
-                <Home />
-                <About />
                 <Projects />
             </main>
             <Footer />
